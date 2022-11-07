a5f7a0db821b32a6d344e56b681c3f2744dc6e42
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-11-07 12:35:05 -0300

    if-else

--- starting/src/Exercises.java
+++ starting/src/Exercises.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Exercises {
 
 	public static void main(String[] args) {	
-//		First exercises
 		Scanner sc = new Scanner(System.in);
+//		Exercise 1
 		
 		System.out.println("Type the radius of the circle:");
 		
@@ -15,6 +15,29 @@ public class Exercises {
 		result = Math.pow(radius, 2) * 3.14;
 		
 		System.out.printf("The area of your circle is: %.2f\n", result);
+		
+//		Exercise 2
+		System.out.println("Let's see how much is the wage of the worker?");
+		
+		int hours_of_work;
+		double earn_per_hour;
+		double monthly_wage;
+		int worked_days;
+
+		System.out.println("First, type how many hours this employee work a day:");
+		hours_of_work = sc.nextInt();
+		
+		System.out.println("Now, How's the earn per hour of this worker?");
+		earn_per_hour = sc.nextDouble();
+		
+		System.out.println("Finally, How many days does th eemployee work per month?");
+		worked_days = sc.nextInt();
+
+
+		monthly_wage = (hours_of_work * earn_per_hour) * worked_days;
+		
+		System.out.printf("\n The employee's monthly wage is: %.2f \n", monthly_wage);
+		
 		sc.close();
 	}
 
---
+++ starting/src/Ifelse.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+//		Working with if else statements
+//		System.out.println("let's play? Write the number of 2 axis(x and y) and I will show you in which quadrant it is");
+//		
+//				
+//		System.out.println("Type the axis 'x':");
+//		int x = sc.nextInt();
+//		
+//		System.out.println("Type the axis 'y':");
+//		int y = sc.nextInt();
+//		
+//		if (x > 0 && y > 0) {
+//			System.out.println("The coordinate is in the 'First Quadrant'!");
+//		}
+//		else if (x < 0 && y > 0) {
+//			System.out.println("The coordinate is in the 'Second Quadrant'!");
+//		}
+//		else if (x < 0 && y < 0) {
+//			System.out.println("The coordinate is in the 'Third Quadrant'!");
+//		}
+//		else if (x > 0 && y > 0) {
+//			System.out.println("The coordinate is in the 'Forth Quadrant'!");
+//		}
+//		else {
+//			System.out.println("The coordinate is in the 'Origin'!");
+//		}
+		
+//		Accumulative attribution
+		
+//		Basic plan: $ 50,00 for 100 minutes
+//		Each minute that is higher than 100 you got to pay $ 2,00
+		System.out.println("Let's see hou much is your plan!");
+		
+		System.out.println("Type how many minutes you have spent on your phone this month:");
+		int minutes = sc.nextInt();
+		
+		double cost = 50.0;
+		
+		if (minutes < 0 ) {
+			System.out.printf("Please, type a number higher tha 0(ZERO)!");
+		}
+		else if (minutes <= 100 ) {
+			System.out.printf("You have to pay $ %.2f! \n", cost);
+		}
+		else {
+			cost += (minutes - 100) * 2;		
+			System.out.printf("You have to pay $ %.2f! \n", cost);
+		}
+		
+		sc.close();
+	}
+
+}
