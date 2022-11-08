619da639606837d5dea04a74300f016e780da243
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-11-08 15:19:05 -0300

    for and while

--- starting/src/repetition_structure/For.java
+++ starting/src/repetition_structure/For.java
@@ -8,36 +8,39 @@ public class For {
 		Scanner sc = new Scanner(System.in);
 
 //		Ex1
-//		
-//		System.out.println("Type a number:");
-//		int number = sc.nextInt();
-//		
-//		for (int i = 1;i <= number; i++) {
-//			if (i % 2 != 0) {
-//				System.out.println(i);
-//			}
-//		}
-//				
+		
+		System.out.println("Type a number:");
+		int number = sc.nextInt();
+		
+		for (int i = 1;i <= number; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+				
 //		EX2 factorial
 		System.out.println("Type an integer number:");
 		int num = sc.nextInt();
-		int factorial = 0;
+		int factorial = num;
 		
 		for (int i = 1; i < num; i++) {
-			factorial += num * i;
+			factorial *= (num - i);
 		}
 		
 		System.out.printf("The factorial of %s is %s", num, factorial);
+		
+//		EX3 Calculating divisors
+		System.out.println("Type an integer number to see its divisors untill itself:");
+		int n = sc.nextInt();
+		
+		for (int i = 1;i <= n;i++) {
+			if (n % i == 0) {
+				System.out.printf("The number is divided entirely by: %s \n", i);
+			}
+		}
 		
 		sc.close();
 		
 	}
 
 }
-
-
-
-
-
-
-
--- starting/src/repetition_structure/While.java
+++ starting/src/repetition_structure/While.java
@@ -7,6 +7,7 @@ public class While {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);	
 		
+//		while
 		System.out.println("Type the password:");
 		int password = sc.nextInt();
 
@@ -16,6 +17,21 @@ public class While {
 		}
 		
 		System.out.printf("Welcome %s \n", password);
+		
+//		Do-While
+		char option;
+		
+		do {
+			System.out.print("Type the temperature in degrees:");
+			double Celsius = sc.nextDouble();
+			double Fahrenheit = 9 * Celsius / 5 + 32;
+			
+			System.out.printf("The equal value in Fahrenheit is %.1f \n", Fahrenheit);
+			System.out.println("Do you want to repete the action? (y/n)");
+			option = sc.next().charAt(0);
+		} while (option != 'n');
+		
+		sc.close();
 	}
 
 }
