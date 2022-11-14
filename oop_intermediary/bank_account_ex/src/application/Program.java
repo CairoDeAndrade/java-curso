e2b591f457785d5f0f67180811bd327f3b042629
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-11-14 18:01:18 -0300

    bank accunt upgrade

--- oop_intermediary/bank_account_ex/src/application/Program.java
+++ oop_intermediary/bank_account_ex/src/application/Program.java
@@ -10,7 +10,8 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+		BankAccount bankAccount;
+
 		System.out.println("Register your bank account here!");
 		System.out.print("Enter your account number: ");
 		int accountNumber = sc.nextInt();
@@ -20,33 +21,34 @@ public class Program {
 		
 		System.out.printf("Do you want to make an innitial deposit?(yes/no)");
 		String answer = sc.next();
-		double balance = 0;
-		BankAccount bankAccount = new BankAccount(accountNumber, name);
 
 		if (answer.equals("yes")) {
 			System.out.println("Enter how much you want to deposit innitialy: ");
-			bankAccount.innitialDeposit = sc.nextDouble();
-			balance = bankAccount.deposit(bankAccount.innitialDeposit);  
+			double initialDeposit = sc.nextDouble();
+			bankAccount = new BankAccount(accountNumber, name, initialDeposit);
 		}
-		bankAccount.showData(balance);
+		else {
+			bankAccount = new BankAccount(accountNumber, name);
+		}
+		System.out.println(bankAccount);
 		
 		System.out.println("Do you want to make a deposit?(yes/no)");
 		answer = sc.next();
 		if (answer.equals("yes")) {
 			System.out.println("Enter how much you want to deposit: ");
 			double quantity = sc.nextDouble();
-			balance += bankAccount.deposit(quantity);
+			bankAccount.deposit(quantity);
 		}
-		bankAccount.showData(balance);
+		System.out.println(bankAccount);
 	
 		System.out.println("Do you want to make a withdraw?(yes/no)");
 		answer = sc.next();
 		if (answer.equals("yes")) {
 			System.out.println("Enter how much you want to withdraw: ");
 			double quantity = sc.nextDouble();
-			balance -= bankAccount.withdraw(quantity);
+			bankAccount.withdraw(quantity);
 		}
-		bankAccount.showData(balance);
+		System.out.println(bankAccount);
 
 		sc.close();
 	}
