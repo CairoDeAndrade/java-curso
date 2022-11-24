6d51af2d99d751c71fb78c96a86a4ada268fc561
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-11-24 19:17:47 -0300

    datetime fundaments 2.0

--- date-hour/date_fundaments/src/application/Program3.java
+++ date-hour/date_fundaments/src/application/Program3.java
@@ -12,26 +12,26 @@ public class Program3 {
 		// for (String s : ZoneId.getAvailableZoneIds())
 		// System.out.println(s);
 
-		LocalDate d04 = LocalDate.parse("2022-07-20");
-		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
-		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+		LocalDate d01 = LocalDate.parse("2022-07-20");
+		LocalDateTime d02 = LocalDateTime.parse("2022-07-20T01:30:26");
+		Instant d03 = Instant.parse("2022-07-20T01:30:26Z");
 
-		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
-		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
-		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
-		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDate r1 = LocalDate.ofInstant(d03, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d03, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d03, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d03, ZoneId.of("Portugal"));
 
 		System.out.println("r1 = " + r1);
 		System.out.println("r2 = " + r2);
 		System.out.println("r3 = " + r3);
 		System.out.println("r4 = " + r4);
 
-		System.out.println("d04 dia = " + d04.getDayOfMonth());
-		System.out.println("d04 mês = " + d04.getMonthValue());
-		System.out.println("d04 ano = " + d04.getYear());
+		System.out.println("d01 day = " + d01.getDayOfMonth());
+		System.out.println("d01 month = " + d01.getMonthValue());
+		System.out.println("d01 year = " + d01.getYear());
 
-		System.out.println("d05 hora = " + d05.getHour());
-		System.out.println("d05 minutos = " + d05.getMinute());
+		System.out.println("d02 hour = " + d02.getHour());
+		System.out.println("d02 minute = " + d02.getMinute());
 	}
 
 }
